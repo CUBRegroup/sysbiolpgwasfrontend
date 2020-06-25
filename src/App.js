3239ba177802a48564b356bfae8dc1c9903417b4
darefalola7 <darefalola7@gmail.com>
2020-06-25 10:48:56 +0100

    Setup temporary page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,15 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import classes from './App.module.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div className={classes.App}>
+      <div className={classes.Soon}>
+        <p>Coming soon...</p>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          This page will host the front end application of SysbiolPGWAS project.
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
     </div>
   );
 }
